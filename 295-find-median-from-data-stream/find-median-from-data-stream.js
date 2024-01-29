f40a1d19c7679b0a215a3ae033cb2a5a52507616
Waqar Ali <chwaqarali6@hotmail.com>
2024-01-29 13:07:52 +0500

    Time: 1235 ms (8.91%) | Memory: 82.7 MB (97.37%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.js
@@ -0,0 +1,53 @@
+
+var MedianFinder = function () {
+    this.arr = []
+};
+
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function (num) {
+    const insertionPoint = getInsertionPoint(this.arr, num)
+    console.log(this.arr.length, insertionPoint)
+    if (insertionPoint === 0) {
+        this.arr.unshift(num)
+    } else if (insertionPoint === this.arr.length) {
+        this.arr.push(num)
+    } else {
+        this.arr.splice(insertionPoint, 0, num)
+    }
+};
+
+const getInsertionPoint = (arr, num) => {
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (arr[mid] == num) return mid;
+        else if (arr[mid] < num) start = mid + 1;
+        else end = mid - 1;
+    }
+
+    return end + 1;
+}
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function () {
+    const length = this.arr.length
+    if (length % 2 === 0) {
+        return (this.arr[length / 2] + this.arr[(length / 2) - 1]) / 2
+    } else {
+        return this.arr[Math.floor(length / 2)]
+    }
+};
+
+/** 
+ * Your MedianFinder object will be instantiated and called as such:
+ * var obj = new MedianFinder()
+ * obj.addNum(num)
+ * var param_2 = obj.findMedian()
+ */
