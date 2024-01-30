2851fdca8fb488af0fe19c39b93dbdcf7d07f412
Waqar Ali <chwaqarali6@hotmail.com>
2024-01-30 23:24:10 +0500

    Time: 48 ms (88.09%) | Memory: 48.9 MB (11.91%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.js
+++ 874-backspace-string-compare/backspace-string-compare.js
@@ -4,31 +4,25 @@
  * @return {boolean}
  */
 var backspaceCompare = function (s, t) {
-    let sPointer = s.length - 1, tPointer = t.length - 1
+    let sPointer = s.length, tPointer = t.length
 
-    while (sPointer >= 0 || tPointer >= 0) {
-        sPointer = getValidChar(s, sPointer)
-        tPointer = getValidChar(t, tPointer)
+    while (sPointer >= 0 && tPointer >= 0) {
+        sPointer = getValidChar(s, sPointer - 1)
+        tPointer = getValidChar(t, tPointer - 1)
 
-        if (sPointer < 0 && tPointer < 0) return true
         if (s[sPointer] !== t[tPointer]) return false
-        if (sPointer < 0 || tPointer < 0) return false
-
-        sPointer = sPointer - 1
-        tPointer = tPointer - 1
     }
 
-    return true
+    return !(sPointer > 0 || tPointer > 0)
 };
 
 function getValidChar(str, curr) {
     let backspaceCount = 0
-    while (curr >= 0) {
+
+    for (; curr >= 0; curr--) {
         if (str[curr] === '#') backspaceCount++
         else if (backspaceCount > 0) backspaceCount--
-        else break
-
-        curr -= 1
+        else return curr
     }
 
     return curr
