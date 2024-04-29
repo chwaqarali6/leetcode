0768e318390fbe7cae2bfca08e0ee8c4194b7e47
Waqar Ali <chwaqarali6@hotmail.com>
2024-04-29 19:32:44 +0500

    Time: 51 ms (67.11%) | Memory: 49.1 MB (41.27%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function (nums) {
+    const counts = [0, 0, 0]
+    nums.forEach(num => counts[num] += 1)
+
+    for (let i = 0; i < nums.length; i++) {
+        if (counts[0] > 0) {
+            nums[i] = 0
+            --counts[0]
+        } else if (counts[1] > 0) {
+            nums[i] = 1
+            --counts[1]
+        } else {
+            nums[i] = 2
+        }
+    }
+
+    return nums
+};
