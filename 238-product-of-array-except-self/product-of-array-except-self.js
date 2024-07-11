b8b48d2ffdeef2c0c220c902e1f1d5e8359d3b6e
Waqar Ali <chwaqarali6@hotmail.com>
2024-07-11 19:07:09 +0500

    Time: 103 ms (60.09%) | Memory: 64 MB (59.38%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -3,22 +3,17 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-    const prefix = [1]
-    const suffix = [1]
+    let prefix = 1, suffix = 1
     const result = []
 
-    for (let i = 1, prod = 1; i < nums.length; i++) {
-        prod *= nums[i - 1]
-        prefix.push(prod)
-    }
-
-    for (let i = nums.length - 2, prod = 1; i >= 0; i--) {
-        prod *= nums[i + 1]
-        suffix.push(prod)
-    }
-
     for (let i = 0; i < nums.length; i++) {
-        result.push(prefix[i] * suffix[nums.length - 1 - i])
+        result[i] = prefix
+        prefix *= nums[i]
+    }
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        result[i] *= suffix
+        suffix *= nums[i]
     }
 
     return result
